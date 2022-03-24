f902649843afb1e81e8c7f1520d3766572b050ce
Giuseppe Salvi <giuseppe.salvi98@gmail.com>
2022-03-24 12:11:23 +0100

    fix: change name to ShowProfileActivity.kt

--- app/src/main/java/it/polito/showprofileactivity/ShowProfileActivity.kt
+++ app/src/main/java/it/polito/showprofileactivity/ShowProfileActivity.kt
@@ -3,7 +3,7 @@ package it.polito.showprofileactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ShowProfileActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
