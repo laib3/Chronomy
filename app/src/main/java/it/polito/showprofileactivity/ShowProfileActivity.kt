177f4c3634fb4370f64c4dc2cd94a4fdc5e243bd
laib3 <54323877+laib3@users.noreply.github.com>
2022-03-25 18:19:22 +0100

    feat: save to and load from shared resources

--- app/src/main/java/it/polito/showprofileactivity/ShowProfileActivity.kt
+++ app/src/main/java/it/polito/showprofileactivity/ShowProfileActivity.kt
@@ -2,10 +2,71 @@ package it.polito.showprofileactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ShowProfileActivity: AppCompatActivity() {
+
+    private val SHPR_NAME:String = "sharedPreferences"
+
+    private val NICKNAME_KEY:String = "nn"
+    private val FULLNAME_KEY:String = "fn"
+    private val BIO_KEY:String = "bio"
+    private val EMAIL_KEY:String = "email"
+    private val PHONE_KEY:String = "phone"
+    private val LOCATION_KEY:String = "loc"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.show_profile_activity)
+
+        // save content to shared resources
+        saveContent("Mario Rossi",
+            "mrossi",
+            "Based in Italy",
+            "mariorossi@gmail.com",
+            "+393001992031",
+            "Miami")
+        // load from shared resources */
+        loadContent()
+
     }
+
+    fun loadContent(){
+        val sharedPreferences = getSharedPreferences(SHPR_NAME, MODE_PRIVATE)
+        val fullNameText:String = sharedPreferences.getString(FULLNAME_KEY, "Full Name") ?: "Full Name"
+        val nickNameText:String = sharedPreferences.getString(NICKNAME_KEY, "NickName") ?: "NickName"
+        val bioText:String = sharedPreferences.getString(BIO_KEY, "bio") ?: "bio"
+        val emailText:String = sharedPreferences.getString(EMAIL_KEY, "email") ?: "email"
+        val phoneNumberText:String = sharedPreferences.getString(PHONE_KEY, "phone") ?: "phone"
+        val locationText:String = sharedPreferences.getString(LOCATION_KEY, "location") ?: "location"
+
+        val tvFullName = findViewById<TextView>(R.id.fullName)
+        val tvNickName = findViewById<TextView>(R.id.nickname)
+        val tvBio = findViewById<TextView>(R.id.bio)
+        val tvEmail = findViewById<TextView>(R.id.email)
+        val tvPhoneNumber = findViewById<TextView>(R.id.phoneNumber)
+        val tvLocation = findViewById<TextView>(R.id.location)
+
+        tvFullName.text = fullNameText
+        tvNickName.text = "@${nickNameText}"
+        tvBio.text = bioText
+        tvEmail.text = emailText
+        tvPhoneNumber.text = phoneNumberText
+        tvLocation.text = locationText
+    }
+
+    fun saveContent(fullName:String, nickName:String, bio:String, email:String, phone:String, location:String){
+        val sharedPreferences = getSharedPreferences(SHPR_NAME, MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        editor.putString(FULLNAME_KEY, fullName)
+        editor.putString(NICKNAME_KEY, nickName)
+        editor.putString(BIO_KEY, bio)
+        editor.putString(EMAIL_KEY, email)
+        editor.putString(PHONE_KEY, phone)
+        editor.putString(LOCATION_KEY, location)
+
+        editor.commit()
+    }
+
 }
