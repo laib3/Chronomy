c5dc193852bfb093a421403310531c6084e4e209
laib3 <54323877+laib3@users.noreply.github.com>
2022-03-25 12:17:20 +0100

    feat: add landscape layout

--- app/src/main/java/it/polito/showprofileactivity/ShowProfileActivity.kt
+++ app/src/main/java/it/polito/showprofileactivity/ShowProfileActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class ShowProfileActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.show_profile_activity)
     }
 }
