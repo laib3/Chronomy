cee3459d96c8f33b61f43716afde3d87eadc5a00
alessandracomparetto <alessandra.comparetto@gmail.com>
2022-04-25 12:24:30 +0200

    feat: timeslot class feat: show data from viewmodel fix: TimeSlotDetailsFragment layout

--- app/src/main/java/it/polito/mainactivity/ui/timeslot_details/TimeSlotDetailsFragment.kt
+++ app/src/main/java/it/polito/mainactivity/ui/timeslot_details/TimeSlotDetailsFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import it.polito.mainactivity.R
 import it.polito.mainactivity.databinding.FragmentTimeslotDetailsBinding
@@ -16,32 +15,40 @@ class TimeSlotDetailsFragment : Fragment() {
 
     private var _binding: FragmentTimeslotDetailsBinding? = null
 
-    private lateinit var tiTitle : TextInputLayout
-    private lateinit var tiDescription : TextInputLayout
-    private lateinit var tiStartDate : TextInputLayout
-    private lateinit var tiEndDate : TextInputLayout
-    private lateinit var tiDuration : TextInputLayout
-    private lateinit var tiLocation : TextInputLayout
-    private lateinit var tiCategory : TextInputLayout
+    private var tiTitle : TextInputLayout? = null
+    private var tiDescription : TextInputLayout? = null
+    private var tiAvailability : TextInputLayout? = null
+    private var tiLocation : TextInputLayout? = null
+    private var tiCategory : TextInputLayout? = null
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val galleryViewModel =
+    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View {
+        val timeSlotDetailsViewModel =
             ViewModelProvider(this).get(TimeSlotDetailsViewModel::class.java)
 
         _binding = FragmentTimeslotDetailsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textTimeslotDetails
-        galleryViewModel.text.observe(viewLifecycleOwner) {
+        timeSlotDetailsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
+        }
+        timeSlotDetailsViewModel.timeslot.observe(viewLifecycleOwner){
+            tiTitle?.editText?.setText(it.title)
+            tiDescription?.editText?.setText(it.description)
+
+            var dateString = ""
+            for (date in it.days){
+                dateString += it.days[0].date.toString()+"/"+it.days[0].month.toString()+ " from "+it.startHour+":"+it.startMinute+" to "+it.endHour+":"+it.endMinute+"\n"
+            }
+            dateString = dateString.substring(0, dateString.length - 1);
+            tiAvailability?.editText?.setText(dateString)
+            tiLocation?.editText?.setText(it.location)
+            tiCategory?.editText?.setText(it.category)
         }
         return root
     }
@@ -49,28 +56,11 @@ class TimeSlotDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //TODO: populate the fields with something!
-        tiTitle = view.findViewById(R.id.TitleTextField)
-        tiTitle.editText?.setText("Il titolo non è molto lungo")
-
-        tiDescription= view.findViewById(R.id.DescriptionTextField)
-        tiDescription.editText?.setText("La descrizione è ancora più super lunga,Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis.")
-
-        tiStartDate = view.findViewById(R.id.StartDateTextField)
-        tiStartDate.editText?.setText("22/04/2022 09:00 AM")
-
-        tiEndDate = view.findViewById(R.id.EndDateTextField)
-        tiEndDate.editText?.setText("22/05/2022 09:00 AM")
-
-        tiDuration = view.findViewById(R.id.DurationTextField)
-        tiDuration.editText?.setText("2h30m")
-
-        tiLocation = view.findViewById(R.id.LocationTextField)
-        tiLocation.editText?.setText("Via Vincenzo Vela 49, 10128, Torino")
-
-        tiCategory = view.findViewById(R.id.CategoryTextField)
-        tiCategory.editText?.setText("Gardening")
-
+        tiTitle = view?.findViewById(R.id.TitleTextField)
+        tiDescription= view?.findViewById(R.id.DescriptionTextField)
+        tiAvailability = view?.findViewById(R.id.AvailabilityTextField)
+        tiLocation = view?.findViewById(R.id.LocationTextField)
+        tiCategory = view?.findViewById(R.id.CategoryTextField)
 
     }
 
