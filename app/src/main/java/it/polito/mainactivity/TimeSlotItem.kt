5a9bbff8b24dd365259af83b9ba77ca548ae5669
alessandracomparetto <alessandra.comparetto@gmail.com>
2022-04-30 16:50:58 +0200

    FIX: timeslot class feat: 4 items list of timeslot

--- app/src/main/java/it/polito/mainactivity/TimeSlotItem.kt
+++ app/src/main/java/it/polito/mainactivity/TimeSlotItem.kt
@@ -6,11 +6,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-data class TimeSlotItem(val title: String, val location: String, val availability: String, val category:String)
-
-
-class TimeSlotItemAdapter(val data: List<TimeSlotItem>): RecyclerView.Adapter<TimeSlotItemAdapter.TimeSlotItemViewHolder>() {
-    class TimeSlotItemViewHolder(v: View): RecyclerView.ViewHolder(v) {
+class TimeslotAdapter(val data: List<Timeslot>): RecyclerView.Adapter<TimeslotAdapter.TimeslotViewHolder>() {
+    class TimeslotViewHolder(v: View): RecyclerView.ViewHolder(v) {
         val title: TextView = v.findViewById(R.id.item_title)
         val location: TextView = v.findViewById(R.id.item_location)
         val availability: TextView = v.findViewById(R.id.item_availability)
@@ -18,17 +15,17 @@ class TimeSlotItemAdapter(val data: List<TimeSlotItem>): RecyclerView.Adapter<Ti
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimeSlotItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimeslotViewHolder {
         val vg = LayoutInflater
             .from(parent.context)
             .inflate(R.layout.timeslot_item, parent, false)
-        return TimeSlotItemViewHolder(vg)
+        return TimeslotViewHolder(vg)
     }
 
-    override fun onBindViewHolder(holder: TimeSlotItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: TimeslotViewHolder, position: Int) {
         holder.title.text = data[position].title
         holder.location.text = data[position].location
-        holder.availability.text = data[position].availability
+        //holder.availability.text = data[position].availability
         holder.category.text = data[position].category
     }
 
--- app/src/main/java/it/polito/mainactivity/ui/timeslot_details/TimeSlotDetailsViewModel.kt
+++ app/src/main/java/it/polito/mainactivity/ui/timeslot_details/TimeSlotDetailsViewModel.kt
@@ -3,21 +3,11 @@ package it.polito.mainactivity.ui.timeslot_details
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import it.polito.mainactivity.Timeslot
 import java.util.*
 import java.time.DayOfWeek
 import kotlin.collections.HashMap
 
-class Timeslot (val title:String,
-                val description:String,
-                val days: List<Date>,
-                val startHour : String,
-                val startMinute: String,
-                val endHour:String,
-                val endMinute:String,
-                val location:String,
-                val category:String ){
-
-}
 
 class TimeSlotDetailsViewModel : ViewModel() {
 
@@ -25,19 +15,5 @@ class TimeSlotDetailsViewModel : ViewModel() {
         value = "Timeslot Details"
     }
     val text: LiveData<String> = _text
-
-
-    private val _timeslot = MutableLiveData<Timeslot>().apply{
-        value = Timeslot("Bring grocery shopping to your door",
-                        "I'll be happy to receive a list of goods to buy for you and to bring it back home to you."+
-                        "I have a car so the quantity is not an issue. You can also select which supermarket you want me to go to,"+
-                        " but please don't choose those outside of the neighbourhood.",
-                        listOf(Date(2022, 4, 25), Date(2022, 5, 6)),
-                        "09", "00", "19", "00",
-                        "New Neighbourhood, Street 10, Sydney",
-                        "Delivery")
-    }
-
-    val timeslot : MutableLiveData<Timeslot> = _timeslot
 
 }
