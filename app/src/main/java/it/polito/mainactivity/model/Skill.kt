5fcc4e432ae47e72103b65576233935915d7b8a4
laib3 <54323877+laib3@users.noreply.github.com>
2022-05-04 12:11:00 +0200

    feat: add UserProfileModel

---
+++ app/src/main/java/it/polito/mainactivity/model/Skill.kt
@@ -0,0 +1,4 @@
+package it.polito.mainactivity.model
+
+class Skill {
+}
---
+++ app/src/test/java/it/polito/mainactivity/model/UserProfileModelTest.kt
@@ -0,0 +1,12 @@
+package it.polito.mainactivity.model
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class UserProfileModelTest {
+
+    @Test
+    fun testToString() {
+    }
+}
