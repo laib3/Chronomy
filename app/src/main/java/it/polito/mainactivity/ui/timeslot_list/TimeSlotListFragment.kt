72f4a4ab057a984b9e6adeaa140feb58fe452645
alessandracomparetto <alessandra.comparetto@gmail.com>
2022-05-05 12:11:50 +0200

    fix: merge between details and main

--- app/src/main/java/it/polito/mainactivity/ui/timeslot_list/TimeSlotListFragment.kt
+++ app/src/main/java/it/polito/mainactivity/ui/timeslot_list/TimeSlotListFragment.kt
@@ -6,19 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import it.polito.mainactivity.R
-import it.polito.mainactivity.Timeslot
 import it.polito.mainactivity.TimeslotAdapter
 import it.polito.mainactivity.databinding.FragmentTimeslotListBinding
-import it.polito.mainactivity.ui.timeslot_edit.TimeSlotEditFragment
-import java.util.*
 
 class TimeSlotListFragment : Fragment() {
 
@@ -28,96 +23,45 @@ class TimeSlotListFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    // TODO: remove
-    private fun createTimeslots(n: Int): List<Timeslot> {
-        /*val l = mutableListOf<Timeslot>()
-        for (i in 1..n) {
-            val i = Timeslot("title$i", "description$i", Date(2022, 2, 12), "09:00 AM", "10:00 PM", "Via qualcosa$i", "skill$i")
-            l.add(i)
-        }
-        return l*/
-        return listOf(
-            Timeslot("Bring grocery shopping to your door",
-                "I'll be happy to receive a list of goods to buy for you and to bring it back home to you."+
-                        "I have a car so the quantity is not an issue. You can also select which supermarket you want me to go to,"+
-                        " but please don't choose those outside of the neighbourhood.",
-                Date(2022, 4, 25),
-                "09:10 AM", "10:00 PM",
-                "New Neighbourhood, Street 10, Sydney",
-                "Delivery") ,
-
-            Timeslot("Walk your dog",
-                "I'll be happy to receive a list of dogs from you and to bring it back home to you."+
-                        "I have a car so the quantity is not an issue. You can also select which supermarket you want me to go to,"+
-                        " but please don't choose those outside of the neighbourhood.",
-                Date(2022, 4, 25),
-                "09:10 AM", "10:00 PM",
-                "New City, Street 10, Anastasia",
-                "Other"),
-
-            Timeslot("Teach to your kid",
-                "I'll be happy to receive a list of goods to buy for you and to bring it back home to you."+
-                        "I have a car so the quantity is not an issue. You can also select which supermarket you want me to go to,"+
-                        " but please don't choose those outside of the neighbourhood.",
-                Date(2022, 4, 25),
-                "09:10 AM", "10:00 PM",
-                "New Neighbourhood, Street 10, Sydney",
-                "Tutoring"),
-
-            Timeslot("Bring grocery YOGA to your door",
-                "I'll be happy to receive a list of goods to buy for you and to bring it back home to you."+
-                        "I have a car so the quantity is not an issue. You can also select which supermarket you want me to go to,"+
-                        " but please don't choose those outside of the neighbourhood.",
-                Date(2022, 4, 25),
-                "09:10 AM", "10:00 PM",
-                "New Neighbourhood, Street 10, Sydney",
-                "Wellness")
-
-        )
-    }
-
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(TimeSlotListViewModel::class.java)
-
         _binding = FragmentTimeslotListBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        //val textView: TextView = binding.textTimeslotList
         val rv: RecyclerView = binding.timeslotListRv
         rv.layoutManager = LinearLayoutManager(root.context)
 
-        val timeslots = createTimeslots(100)
-        val adapter = TimeslotAdapter(timeslots, this)
-        rv.adapter = adapter
+        val timeSlotListViewModel = ViewModelProvider(this)[TimeSlotListViewModel::class.java]
+        timeSlotListViewModel.timeslots.observe(viewLifecycleOwner) {
 
-        // If the list of timeslots is empty, show a message
-        val tv: TextView = binding.emptyTimeslotListMessage
-        tv.visibility = if (adapter.itemCount == 0)
-            View.VISIBLE
-        else
-            View.INVISIBLE
+            val adapter = TimeslotAdapter(it, this)
+            rv.adapter = adapter
+
+            // If the list of timeslots is empty, show a message
+            val tv: TextView = binding.emptyTimeslotListMessage
+            tv.visibility = if (adapter.itemCount == 0)
+                View.VISIBLE
+            else
+                View.INVISIBLE
+        }
 
 
         // If click on fab, go to Edit timeslot
-        val fab : FloatingActionButton = binding.fab
+        val fab: FloatingActionButton = binding.fab
 
         var bundle = Bundle();
         bundle.putInt("id", -1)
 
-        fab.setOnClickListener { findNavController().navigate(R.id.action_nav_list_to_nav_edit, bundle)}
-
-        // TODO
-        /*homeViewModel.text.observe(viewLifecycleOwner) {
-            rv.text = it
+        fab.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_nav_list_to_nav_edit,
+                bundle
+            )
         }
-        é/
-         */
+
         return root
     }
 
