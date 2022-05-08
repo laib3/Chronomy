6ad1a5b646ae85dbecb70d93a22afd45ea3f0b1d
Giuseppe Salvi <giuseppe.salvi98@gmail.com>
2022-05-09 00:43:35 +0200

    feat: display dates and hours

--- app/src/main/java/it/polito/mainactivity/ui/timeslot/timeslot_list/TimeslotAdapter.kt
+++ app/src/main/java/it/polito/mainactivity/ui/timeslot/timeslot_list/TimeslotAdapter.kt
@@ -13,21 +13,20 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.card.MaterialCardView
 import it.polito.mainactivity.R
-import it.polito.mainactivity.model.Timeslot
 import it.polito.mainactivity.model.Utils
 import it.polito.mainactivity.ui.timeslot.TimeslotViewModel
-import java.util.*
 
 
-class TimeslotAdapter(val vm: TimeslotViewModel, val parentFragment: Fragment): RecyclerView.Adapter<TimeslotAdapter.TimeslotViewHolder>() {
+class TimeslotAdapter(private val vm: TimeslotViewModel, private val parentFragment: Fragment) :
+    RecyclerView.Adapter<TimeslotAdapter.TimeslotViewHolder>() {
 
-    class TimeslotViewHolder(v: View): RecyclerView.ViewHolder(v) {
+    class TimeslotViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         val tvTitle: TextView = v.findViewById(R.id.tvTitle)
         val tvLocation: TextView = v.findViewById(R.id.tvLocation)
         val tvDate: TextView = v.findViewById(R.id.tvDate)
         val tvHour: TextView = v.findViewById(R.id.tvHour)
         val ivCategory: ImageView = v.findViewById(R.id.ivCategory)
-        val cvTimeslotCard: MaterialCardView= v.findViewById(R.id.cvTimeslotCard)
+        val cvTimeslotCard: MaterialCardView = v.findViewById(R.id.cvTimeslotCard)
         val ibEdit: ImageButton = v.findViewById(R.id.ibEdit)
         val ibDelete: ImageButton = v.findViewById(R.id.ibDelete)
     }
@@ -40,38 +39,52 @@ class TimeslotAdapter(val vm: TimeslotViewModel, val parentFragment: Fragment): 
     }
 
     override fun onBindViewHolder(holder: TimeslotViewHolder, position: Int) {
-        holder.tvTitle.text = vm.timeslots.value?.get(position)?.title
-        holder.tvLocation.text = vm.timeslots.value?.get(position)?.location
+        val ts = vm.timeslots.value?.get(position)
+        holder.tvTitle.text = ts?.title
+        holder.tvLocation.text = ts?.location
 
-        // TODO: if repetition, decide how to display it and do it
-        holder.tvDate.text = Utils.formatDateToString(vm.timeslots.value?.get(position)?.date)
-
-        holder.tvHour.text =  "${vm.timeslots.value?.get(position)?.startHour} - ${vm.timeslots.value?.get(position)?.endHour}"
+        holder.tvDate.text = when (ts?.repetition) {
+            "weekly" -> "from " + Utils.formatDateToString(ts.date) +
+                    " until " + Utils.formatDateToString(ts.date) +
+                    "\nevery week"
+            "monthly" -> "from " + Utils.formatDateToString(ts.date) +
+                    " until " + Utils.formatDateToString(ts.date) +
+                    "\nevery month"
+            else -> Utils.formatDateToString(vm.timeslots.value?.get(position)?.date)
+        }
+        holder.tvHour.text =
+            parentFragment.activity?.getString(
+                R.string.starting_hour_dash_ending_hour,
+                ts?.startHour,
+                ts?.endHour
+            )
 
         // Change the image icon of the skill with the correct one
-        Utils.getSkillImgRes(vm.timeslots.value?.get(position)!!.category)?.also{ it -> holder.ivCategory.setImageResource(it)}
+        Utils.getSkillImgRes(vm.timeslots.value?.get(position)!!.category)
+            ?.also { it -> holder.ivCategory.setImageResource(it) }
 
         // Pass through bundle the id of the item in the list
-        var bundle = Bundle();
+        val bundle = Bundle()
         bundle.putInt("id", position)
 
         // click on card, show details of that item
-        holder.cvTimeslotCard.setOnClickListener{
+        holder.cvTimeslotCard.setOnClickListener {
             parentFragment.findNavController().navigate(R.id.action_nav_list_to_nav_details, bundle)
         }
         // click on edit button, edit details of that item
-        holder.ibEdit.setOnClickListener{
+        holder.ibEdit.setOnClickListener {
             parentFragment.findNavController().navigate(R.id.action_nav_list_to_nav_edit, bundle)
         }
         // click on delete button, show modal to confirm the deletion of that item
-        holder.ibDelete.setOnClickListener{
-            // TODO: open the modal
+        holder.ibDelete.setOnClickListener {
             android.app.AlertDialog.Builder(parentFragment.requireContext())
                 .setTitle("Delete Timeslot")
-                .setMessage("Are you sure you want to delete this timeslot?\n\n" +
-                        "\'${vm.timeslots.value?.get(position)?.title}\'\n")
+                .setMessage(
+                    "Are you sure you want to delete this timeslot?\n\n" +
+                            "\'${vm.timeslots.value?.get(position)?.title}\'\n"
+                )
                 .setPositiveButton("Delete", DialogInterface.OnClickListener(
-                    fun (_, _){
+                    fun(_, _) {
                         vm.removeTimeslot(position)
                     }
 
@@ -79,9 +92,8 @@ class TimeslotAdapter(val vm: TimeslotViewModel, val parentFragment: Fragment): 
                 .setNegativeButton("Cancel", null)
                 .setIcon(R.drawable.ic_baseline_report_problem_24)
                 .show()
-            }
-            // TODO: if confirmed, delete the element at position from the list
         }
+    }
 
     override fun getItemCount(): Int = vm.timeslots.value?.size ?: 0
 
