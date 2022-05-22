20583b21f1d6a38aa91867432cacc28a2eb6ab40
alessandracomparetto <alessandra.comparetto@gmail.com>
2022-05-22 11:36:35 +0200

    fix: guidelines fix: fragment filters

--- app/src/main/java/it/polito/mainactivity/ui/home/FiltersDialogFragment.kt
+++ app/src/main/java/it/polito/mainactivity/ui/home/FiltersDialogFragment.kt
@@ -5,8 +5,6 @@ import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.app.Dialog
 import android.app.TimePickerDialog
-import android.content.DialogInterface
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -16,14 +14,9 @@ import android.widget.TextView
 import android.widget.TimePicker
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.google.android.material.snackbar.Snackbar
 import com.skydoves.expandablelayout.ExpandableLayout
-import it.polito.mainactivity.MainActivity
 import it.polito.mainactivity.R
 import it.polito.mainactivity.databinding.FragmentFiltersBottomDialogBinding
-import it.polito.mainactivity.model.Utils
-import java.text.DateFormat
-import java.text.SimpleDateFormat
 import java.util.*
 
 private var MINDATE:Long = GregorianCalendar.getInstance().timeInMillis
@@ -36,7 +29,7 @@ class FiltersDialogFragment(): BottomSheetDialogFragment() {
     private var _binding: FragmentFiltersBottomDialogBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View {
         _binding = FragmentFiltersBottomDialogBinding.inflate(inflater, container, false)
         val root: View = binding.root
         val expandableDate= binding.expandableDate
@@ -44,26 +37,24 @@ class FiltersDialogFragment(): BottomSheetDialogFragment() {
         val expandableDuration = binding.expandableDuration
 
         expandableDate.parentLayout.setOnClickListener{toggleExpandable(expandableDate)}
-        expandableHour.parentLayout.setOnClickListener{toggleExpandable(expandableHour)}
-        expandableDuration.parentLayout.setOnClickListener{toggleExpandable(expandableDuration)}
+       expandableHour.parentLayout.setOnClickListener{toggleExpandable(expandableHour)}
+       expandableDuration.parentLayout.setOnClickListener{toggleExpandable(expandableDuration)}
 
         return root
     }
 
     private fun toggleExpandable(ex: ExpandableLayout){
         if(ex.isExpanded){
-            ex.parentLayout.setBackgroundResource(R.drawable.searchview_background)
             ex.collapse()
         }else{
-            ex.parentLayout.setBackgroundResource(R.drawable.expandable_parent_background)
             ex.expand()
         }
     }
 
     override fun onResume() {
-        var secondLayoutDate = binding.expandableDate.secondLayout
-        var tvStartDate = secondLayoutDate.findViewById<TextView>(R.id.tvStartDate)
-        var tvEndDate = secondLayoutDate.findViewById<TextView>(R.id.tvEndDate)
+        val secondLayoutDate = binding.expandableDate.secondLayout
+        val tvStartDate = secondLayoutDate.findViewById<TextView>(R.id.tvStartDate)
+        val tvEndDate = secondLayoutDate.findViewById<TextView>(R.id.tvEndDate)
 
         tvStartDate.setOnClickListener{
             DatePickerFragment(tvStartDate, DatePickerFragment.DType.START, MINDATE).show(requireActivity().supportFragmentManager, "startDatePicker")
@@ -73,9 +64,9 @@ class FiltersDialogFragment(): BottomSheetDialogFragment() {
             DatePickerFragment(tvEndDate, DatePickerFragment.DType.END, MINDATE).show(requireActivity().supportFragmentManager, "endDatePicker")
         }
 
-        var secondLayoutHour = binding.expandableHour.secondLayout
-        var tvStartTime = secondLayoutHour.findViewById<TextView>(R.id.tvStartTime)
-        var tvEndTime = secondLayoutHour.findViewById<TextView>(R.id.tvEndTime)
+       val secondLayoutHour = binding.expandableHour.secondLayout
+        val tvStartTime = secondLayoutHour.findViewById<TextView>(R.id.tvStartTime)
+        val tvEndTime = secondLayoutHour.findViewById<TextView>(R.id.tvEndTime)
 
         tvStartTime.setOnClickListener{
             TimePickerFragment(tvStartTime, TimePickerFragment.HType.START, MINHOUR, MINHOUR, binding.root).show(requireActivity().supportFragmentManager, "startTimePicker")
@@ -102,9 +93,9 @@ class FiltersDialogFragment(): BottomSheetDialogFragment() {
 
         override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
             // Use the current date as the default date in the picker only if it's the first time opening
-            var year = 0
-            var month = 0
-            var day = 0
+            var year: Int
+            var month:Int
+            var day:Int
             val c = Calendar.getInstance()
             if(tvDate.text != "dd/mm/yyyy"){
                 val list = tvDate.text.toString().split("/")
@@ -129,7 +120,7 @@ class FiltersDialogFragment(): BottomSheetDialogFragment() {
 
         @SuppressLint("SetTextI18n")
         override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
-            tvDate?.text = "${day}/${(month + 1)}/${year}"
+            tvDate.text = "${day}/${(month + 1)}/${year}"
             if(type == DType.START){
                 val cal  = GregorianCalendar.getInstance()
                     cal.set(year, month, day)
