b26e48b5d819e399bf6fc6f8c338ddfd0ebf5419
alessandracomparetto <alessandra.comparetto@gmail.com>
2022-05-29 16:12:29 +0200

    feat: request item layout

--- app/src/main/java/it/polito/mainactivity/ui/request/RequestRecyclerViewAdapter.kt
+++ app/src/main/java/it/polito/mainactivity/ui/request/RequestRecyclerViewAdapter.kt
@@ -7,10 +7,13 @@ import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.DiffUtil.calculateDiff
-import com.google.android.material.card.MaterialCardView
+import androidx.transition.AutoTransition
+import androidx.transition.TransitionManager
+import com.skydoves.expandablelayout.ExpandableLayout
 import it.polito.mainactivity.R
 
 import it.polito.mainactivity.model.Timeslot
@@ -22,47 +25,57 @@ class RequestRecyclerViewAdapter(
 ) : RecyclerView.Adapter<RequestRecyclerViewAdapter.RequestViewHolder>() {
 
    inner class RequestViewHolder(v: View) : RecyclerView.ViewHolder(v) {
-        val tvTitle: TextView = v.findViewById(R.id.tvTitle)
-        val tvLocation: TextView = v.findViewById(R.id.tvLocation)
-        val tvDate: TextView = v.findViewById(R.id.tvDate)
-        val tvHour: TextView = v.findViewById(R.id.tvHour)
-        val ivCategory: ImageView = v.findViewById(R.id.lCategory)
-        val cvTimeslotCard: MaterialCardView = v.findViewById(R.id.cvTimeslotCard)
+       val tvTitle: TextView = v.findViewById(R.id.tvTitle)
+       val ivCategory: ImageView = v.findViewById(R.id.lCategory)
+       val btnArrow:ImageButton = v.findViewById(R.id.arrow_button)
+       val insideContent:ConstraintLayout = v.findViewById(R.id.hidden_view)
+       val baseCardView : CardView = v.findViewById(R.id.base_cardview)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RequestViewHolder {
         val vg = LayoutInflater
             .from(parent.context)
-            .inflate(R.layout.timeslot_item, parent, false)
+            .inflate(R.layout.request_item, parent, false)
         return RequestViewHolder(vg)
     }
 
     override fun onBindViewHolder(holder: RequestViewHolder, position: Int) {
         val ts = values[position]
         holder.tvTitle.text = ts.title
-        holder.tvLocation.text = ts.location
-        holder.tvDate.text = when (ts.repetition) {
-            "Weekly" -> "from " + Utils.formatDateToString(ts.startDate) +
-                    " until " + Utils.formatDateToString(ts.endRepetitionDate) +
-                    "\nevery week"
-            "Monthly" -> "from " + Utils.formatDateToString(ts.startDate) +
-                    " until " + Utils.formatDateToString(ts.endRepetitionDate) +
-                    "\nevery month"
-            else -> Utils.formatDateToString(ts.startDate)
-        }
-
-        holder.tvHour.text =
-            parentFragment.activity?.getString(
-                R.string.starting_hour_dash_ending_hour,
-                ts.startHour,
-                ts.endHour,
-                Utils.getDuration(ts.startHour, ts.endHour)
-            )
-
-        // Change the image icon of the skill with the correct one
-        Utils.getSkillImgRes(ts.category)
+       Utils.getSkillImgRes(ts.category)
             ?.also { it -> holder.ivCategory.setImageResource(it) }
-        
+
+
+       holder.btnArrow.setOnClickListener {
+           // If the CardView is already expanded, set its visibility
+           //  to gone and change the expand less icon to expand more.
+           if (holder.insideContent.getVisibility() == View.VISIBLE) {
+
+               // The transition of the hiddenView is carried out
+               //  by the TransitionManager class.
+               // Here we use an object of the AutoTransition
+               // Class to create a default transition.
+               TransitionManager.beginDelayedTransition(
+                   holder.baseCardView,
+                   AutoTransition()
+               );
+
+               holder.insideContent.visibility = View.GONE;
+               holder.btnArrow.setImageResource(R.drawable.ic_arrow_down);
+           }
+
+           // If the CardView is not expanded, set its visibility
+           // to visible and change the expand more icon to expand less.
+           else {
+
+               TransitionManager.beginDelayedTransition(
+                   holder.baseCardView,
+                   AutoTransition());
+               holder.insideContent.visibility = View.VISIBLE;
+               holder.btnArrow.setImageResource(R.drawable.ic_arrow_up);
+           }
+       }
+
         // TODO: click on card, show chats preview
         /*
         holder.cvTimeslotCard.setOnClickListener {
@@ -75,7 +88,7 @@ class RequestRecyclerViewAdapter(
 
     fun filterList(filteredList: List<Timeslot>) {
         val diffUtil = RequestDiffUtil(values, filteredList)
-        val diffResult = DiffUtil.calculateDiff(diffUtil)
+        val diffResult = calculateDiff(diffUtil)
         values = filteredList
         diffResult.dispatchUpdatesTo(this)
 
