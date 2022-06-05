09d6d2fa48c9a58d743c6af7283a8abb91bc33ab
Giuseppe Salvi <giuseppe.salvi98@gmail.com>
2022-06-06 00:50:45 +0200

    feat: Rating adapter populated with values from vm

--- app/src/main/java/it/polito/mainactivity/ui/userprofile/showprofile/ShowProfileRatingsFragment.kt
+++ app/src/main/java/it/polito/mainactivity/ui/userprofile/showprofile/ShowProfileRatingsFragment.kt
@@ -1,37 +1,28 @@
 package it.polito.mainactivity.ui.userprofile.showprofile
 
-import android.content.DialogInterface
 import android.os.Bundle
 import android.view.*
-import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
-import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.card.MaterialCardView
-import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.firestore.FirebaseFirestoreException
-import it.polito.mainactivity.MainActivity
+import com.squareup.picasso.Picasso
 import it.polito.mainactivity.R
-import it.polito.mainactivity.databinding.FragmentShowProfileInfoBinding
 import it.polito.mainactivity.databinding.FragmentShowProfileRatingsBinding
-import it.polito.mainactivity.databinding.FragmentShowProfileSkillsBinding
-import it.polito.mainactivity.model.Rating
-import it.polito.mainactivity.model.Timeslot
+import it.polito.mainactivity.model.*
 
-import it.polito.mainactivity.model.Utils
-import it.polito.mainactivity.ui.userprofile.SkillCard
 import it.polito.mainactivity.viewModel.TimeslotViewModel
-import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
+import it.polito.mainactivity.viewModel.UserProfileViewModel
+import kotlin.math.roundToInt
 
 class ShowProfileRatingsFragment(val timeslotId: String?) : Fragment() {
     private var _binding: FragmentShowProfileRatingsBinding? = null
     private val binding get() = _binding!!
     private val vmTimeslots: TimeslotViewModel by activityViewModels()
+    private val vmUser: UserProfileViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,10 +32,68 @@ class ShowProfileRatingsFragment(val timeslotId: String?) : Fragment() {
 
         _binding = FragmentShowProfileRatingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        // If show profile of other users
-        if (timeslotId != null) {
 
+        // If show profile of other users
+        var currentUserId: String? = null
+        var currentUserNickname: String? = null
+        var currentUserProfilePictureUrl: String? = null
+        if (timeslotId != null) {
+            vmTimeslots.timeslots.value?.find { t -> t.timeslotId == timeslotId }
+                ?.also {
+                    currentUserId = it.publisher["userId"] as String
+                    currentUserNickname = it.publisher["nickname"] as String
+                    currentUserProfilePictureUrl = it.publisher["profilePictureUrl"] as String
+                }
         } else { // if show profile of the current publisher
+            currentUserId = vmUser.user.value?.userId!!
+            currentUserNickname = vmUser.user.value?.nickname!!
+            currentUserProfilePictureUrl = vmUser.user.value?.profilePictureUrl!!
+        }
+
+
+        val rbAvgRatingPublisher: RatingBar = binding.rbAvgRatingPublisher
+        val rbAvgRatingClient: RatingBar = binding.rbAvgRatingClient
+        val rvPublisher: RecyclerView = binding.rvCommentsPublisher
+        val rvClient: RecyclerView = binding.rvCommentsClient
+        rvPublisher.layoutManager = LinearLayoutManager(root.context)
+        rvClient.layoutManager = LinearLayoutManager(root.context)
+
+        vmTimeslots.timeslots.observe(viewLifecycleOwner) {
+
+            val ratingsAsPublisher: MutableList<RatingWithUserInfo> = mutableListOf()
+            it.filter { timeslot ->
+                (timeslot.publisher["userId"] == currentUserId) &&
+                        (timeslot.status == Timeslot.Status.COMPLETED) &&
+                        (timeslot.ratings.find { rating -> rating.by == Message.Sender.CLIENT }?.rating != -1)
+            }
+                .map { timeslot ->
+                    RatingWithUserInfo(timeslot.ratings.filter { rating -> rating.by == Message.Sender.CLIENT }
+                        .first(), currentUserNickname, currentUserProfilePictureUrl)
+                }
+
+            val ratingsAsClient: MutableList<RatingWithUserInfo> = mutableListOf()
+            it.filter { timeslot ->
+                timeslot.publisher["userId"] != currentUserId &&
+                        timeslot.chats.filter { chat -> chat.assigned }
+                            .map { chat -> chat.client["userId"] }[0] == currentUserId &&
+                        timeslot.status == Timeslot.Status.COMPLETED &&
+                        timeslot.ratings.find { rating -> rating.by == Message.Sender.PUBLISHER }?.rating != -1
+            }
+                .map { timeslot ->
+                    RatingWithUserInfo(timeslot.ratings.filter { rating -> rating.by == Message.Sender.PUBLISHER }
+                        .first(), currentUserNickname, currentUserProfilePictureUrl)
+                }
+
+            val publisherListAdapter = RatingAdapter(ratingsAsPublisher, this)
+            rvPublisher.adapter = publisherListAdapter
+
+            val clientListAdapter = RatingAdapter(ratingsAsClient, this)
+            rvClient.adapter = clientListAdapter
+
+            rbAvgRatingPublisher.numStars =
+                ratingsAsPublisher.map { r -> r.rating.rating }.average().roundToInt()
+            rbAvgRatingClient.numStars =
+                ratingsAsClient.map { r -> r.rating.rating }.average().roundToInt()
 
         }
         return root
@@ -57,20 +106,16 @@ class ShowProfileRatingsFragment(val timeslotId: String?) : Fragment() {
 }
 
 class RatingAdapter(
-    private val ratingsList: List<Rating>,
+    private val ratingsList: List<RatingWithUserInfo>,
     private val parentFragment: Fragment,
 ) :
     RecyclerView.Adapter<RatingAdapter.RatingViewHolder>() {
 
     class RatingViewHolder(v: View) : RecyclerView.ViewHolder(v) {
-        //val tvTitle: TextView = v.findViewById(R.id.tvTitle)
-        //val tvLocation: TextView = v.findViewById(R.id.tvLocation)
-        //val tvDate: TextView = v.findViewById(R.id.tvDate)
-        //val tvHour: TextView = v.findViewById(R.id.tvHour)
-        //val ivCategory: ImageView = v.findViewById(R.id.lCategory)
-        //val cvTimeslotCard: MaterialCardView = v.findViewById(R.id.cvTimeslotCard)
-        //val ibEdit: ImageButton = v.findViewById(R.id.ibEdit)
-        //val ibDelete: ImageButton = v.findViewById(R.id.ibDelete)
+        val tvUserNickname: TextView = v.findViewById(R.id.tvUserNickname)
+        val tvComment: TextView = v.findViewById(R.id.tvComment)
+        val ratingBar: RatingBar = v.findViewById(R.id.rbRating)
+        val userPic: ImageView = v.findViewById(R.id.iUserPic)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RatingViewHolder {
@@ -81,55 +126,12 @@ class RatingAdapter(
     }
 
     override fun onBindViewHolder(holder: RatingViewHolder, position: Int) {
-        //val ts = myTimeslots[position]
-        //holder.tvTitle.text = ts.title
-        //holder.tvLocation.text = ts.location
-        //holder.tvDate.text = Utils.formatDateToString(ts.date)
+        val rating = ratingsList[position]
 
-        //holder.tvHour.text =
-        //    parentFragment.activity?.getString(
-        //        R.string.starting_hour_dash_ending_hour,
-        //        ts.startHour,
-        //        ts.endHour,
-        //        Utils.getDuration(ts.startHour, ts.endHour)
-        //    )
-
-        // Change the image icon of the skill with the correct one
-        //Utils.getSkillImgRes(ts.category)
-        //    ?.also { it -> holder.ivCategory.setImageResource(it) }
-
-        //// Pass through bundle the id of the item in the list
-        //val bundle = Bundle()
-        //bundle.putString("id", ts.timeslotId)
-        //bundle.putBoolean("showOnly", false)
-        //bundle.putBoolean("startChat", false)
-
-        //// click on card, show details of that item
-        //holder.cvTimeslotCard.setOnClickListener {
-        //    parentFragment.findNavController().navigate(R.id.action_nav_list_to_nav_details, bundle)
-        //}
-        //// click on edit button, edit details of that item
-        //holder.ibEdit.setOnClickListener {
-        //    parentFragment.findNavController().navigate(R.id.action_nav_list_to_nav_edit, bundle)
-        //}
-        //// click on delete button, show modal to confirm the deletion of that item
-        //holder.ibDelete.setOnClickListener {
-        //    android.app.AlertDialog.Builder(parentFragment.requireContext())
-        //        .setTitle("Delete Timeslot")
-        //        .setMessage(
-        //            "Are you sure you want to delete this timeslot?\n\n" +
-        //                    "\'${ts.title}\'\n"
-        //        )
-        //        .setPositiveButton("Delete", DialogInterface.OnClickListener(
-        //            fun(_, _) {
-        //                vm.deleteTimeslot(ts.timeslotId)
-        //            }
-
-        //        ))
-        //        .setNegativeButton("Cancel", null)
-        //        .setIcon(R.drawable.ic_baseline_report_problem_24)
-        //        .show()
-        //}
+        holder.tvUserNickname.text = rating.nickname
+        holder.tvComment.text = rating.rating.comment
+        holder.ratingBar.numStars = rating.rating.rating
+        Picasso.get().load(rating.profilePictureUrl).into(holder.userPic)
     }
 
     override fun getItemCount(): Int = ratingsList.size
