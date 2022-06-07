135db3fb5ae8e2ec6cbb8506da17b053b17d2f0b
Giuseppe Salvi <giuseppe.salvi98@gmail.com>
2022-06-07 22:50:54 +0200

    feat: disable rating area when sent

--- app/src/main/java/it/polito/mainactivity/ui/chat/ChatFragment.kt
+++ app/src/main/java/it/polito/mainactivity/ui/chat/ChatFragment.kt
@@ -3,6 +3,8 @@ package it.polito.mainactivity.ui.chat
 import android.app.Activity
 import android.content.Context
 import android.content.Context.INPUT_METHOD_SERVICE
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -51,10 +53,10 @@ class ChatFragment : Fragment() {
         vm.timeslots.observe(viewLifecycleOwner) { timeslots ->
             ts = timeslots?.firstOrNull { it.timeslotId == args.timeslotId }
             chat = ts?.chats?.firstOrNull { it.chatId == args.chatId }
-            if(chat == null)
+            if (chat == null)
                 findNavController().navigateUp()
             else {
-                if(ts == null)
+                if (ts == null)
                     findNavController().navigateUp()
                 else {
                     adapter = MessageRecyclerViewAdapter(
@@ -62,6 +64,25 @@ class ChatFragment : Fragment() {
                     )
                     rv.adapter = adapter
                     rv.scrollToPosition(chat!!.messages.size - 1);
+
+
+                    val userRole = Utils.getUserRole(ts!!, chat!!)
+                    val userRating = ts!!.ratings.find { r -> r.by == userRole }?.rating
+                    val userRatingComment = ts!!.ratings.find { r -> r.by == userRole }?.comment
+                    if (userRating != null && userRating != -1) {
+                        binding.ratingBar.rating = userRating.toFloat()
+                        binding.ratingBar.isEnabled = false
+                        binding.textInputEditTextSkillDescription.setText(userRatingComment)
+                        binding.textInputEditTextSkillDescription.isEnabled = false
+                        binding.btnSendFeedback.isEnabled = false
+                        binding.btnSendFeedback.text = "Feedback sent"
+                        binding.btnSendFeedback.setBackgroundColor(resources.getColor(R.color.dark_grey))
+                        binding.btnSendFeedback.setTextColor(
+                            ColorStateList.valueOf(
+                                requireParentFragment().resources.getColor(R.color.white)
+                            )
+                        )
+                    }
 
                     when (ts!!.status) {
                         Timeslot.Status.PUBLISHED -> {
@@ -80,7 +101,7 @@ class ChatFragment : Fragment() {
                             binding.btnsManageReq.visibility = View.GONE
                             binding.clCompleted.visibility = View.VISIBLE
                             binding.ratingZone.visibility = View.GONE
-                            if(!(chat!!.assigned)){ // timeslot has been assigned to someone else
+                            if (!(chat!!.assigned)) { // timeslot has been assigned to someone else
                                 binding.cvAlreadyAssigned.visibility = View.VISIBLE
                                 binding.textMsg.visibility = View.GONE
                                 binding.bSendMsg.visibility = View.GONE
@@ -88,9 +109,9 @@ class ChatFragment : Fragment() {
                         }
                         else -> {}
                     }
-                    if(Utils.getUserRole(ts!!, chat!!) == Message.Sender.CLIENT)
+                    if (Utils.getUserRole(ts!!, chat!!) == Message.Sender.CLIENT)
                         binding.btnsManageReq.visibility = View.GONE
-                        //binding.clCompleted.visibility = View.GONE
+                    //binding.clCompleted.visibility = View.GONE
                 }
 
                 binding.bSendMsg.setOnClickListener {
@@ -98,7 +119,7 @@ class ChatFragment : Fragment() {
                     binding.textMsg.setText("")
                 }
 
-                binding.bReject.setOnClickListener{
+                binding.bReject.setOnClickListener {
                     vm.reject(chat?.chatId)
                     findNavController().navigateUp()
                 }
@@ -107,16 +128,20 @@ class ChatFragment : Fragment() {
                     vm.setChatAssigned(chat!!.chatId, true)
                 }
 
-                binding.bCompleted.setOnClickListener{
-                    vm.updateTimeslotField(ts!!.timeslotId, "status", Timeslot.Status.COMPLETED )
+                binding.bCompleted.setOnClickListener {
+                    vm.updateTimeslotField(ts!!.timeslotId, "status", Timeslot.Status.COMPLETED)
                 }
 
                 binding.btnSendFeedback.setOnClickListener {
-                    vm.updateRating(ts!!.timeslotId, binding.ratingBar.rating.toInt(), binding.textInputComment.editText?.text.toString())
+                    vm.updateRating(
+                        ts!!.timeslotId,
+                        binding.ratingBar.rating.toInt(),
+                        binding.textInputComment.editText?.text.toString()
+                    )
                 }
 
-                }
             }
+        }
         return root
     }
 
